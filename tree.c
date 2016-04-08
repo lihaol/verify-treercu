d42ab8a4bf6f4877ba14741096395d15aaf2d406
Lihao Liang <lihao.liang@cs.ox.ac.uk>
2016-04-08 22:28:11 +0100

    use IS_ENABLED()

--- tree.c
+++ tree.c
@@ -4298,12 +4298,11 @@ static int __init rcu_spawn_gp_kthread(void)
 			sp.sched_priority = kthread_prio;
 			sched_setscheduler_nocheck(t, SCHED_FIFO, &sp);
 		}
-#if defined(CBMC) || defined(RUN)
-		bool ret = rcu_gp_init(rsp);
-		//assert(ret);
-#else
-		wake_up_process(t);
-#endif
+		if (IS_ENABLED(CBMC) || IS_ENABLED(RUN)) {
+			bool ret = rcu_gp_init(rsp);
+			//assert(ret);
+		} else 
+			wake_up_process(t);
 		raw_spin_unlock_irqrestore(&rnp->lock, flags);
 	}
 	rcu_spawn_nocb_kthreads();
