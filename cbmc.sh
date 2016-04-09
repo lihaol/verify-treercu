54454e315d6bd1be365b59908af7e3b80024ec02
Lihao Liang <lihao.liang@cs.ox.ac.uk>
2016-04-09 22:37:34 +0100

    update cbmc run script

--- cbmc.sh
+++ cbmc.sh
@@ -1,6 +1,6 @@
 # need cbmc v5.0
-~/cbmc-releases/5.3/cbmc --unwind 5  -I . -DCBMC -DPER_CPU_DATA_ARRAY -DFORCE_FAILURE_2 main.c
+~/cbmc-releases/5.3/cbmc --unwind 3  -I . -DCBMC -DPER_CPU_DATA_ARRAY -DFORCE_FAILURE_2 main.c
 # Can add "--mm pso" or "--arch powerpc" for some variety.
 # -DFORCE_FAILURE to force failure (bogus dynticks failure)
-# -DFORCE_FAILURE_2 to force failure (drop synchronize_rcu(), ineffective!)
+# -DFORCE_FAILURE_2 to force failure (drop synchronize_rcu())
 # -DCBMC_ORDERING_BUG to do unsynchronized accesses to noassert
