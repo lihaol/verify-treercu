b1c63ca015b070e348ebf4930306ba602947d038
Lihao Liang <lihao.liang@cs.ox.ac.uk>
2016-04-22 01:29:15 +0100

    Use CBMC 5.4

--- cbmc.sh
+++ cbmc.sh
@@ -1,5 +1,5 @@
 # need cbmc v5.0
-~/cbmc-releases/5.3/cbmc --unwind 3  -I . -DCBMC -DPER_CPU_DATA_ARRAY -DFORCE_FAILURE_2 main.c
+~/cbmc-releases/5.4/cbmc --unwind 3  -I . -DCBMC -DPER_CPU_DATA_ARRAY -DFORCE_FAILURE_2 main.c
 # Can add "--mm pso" or "--arch powerpc" for some variety.
 # -DFORCE_FAILURE to force failure (bogus dynticks failure)
 # -DFORCE_FAILURE_2 to force failure (drop synchronize_rcu())
