5b3ec7e627eeaf60348720481ad89caa1c40ff78
Lihao Liang <lihao.liang@cs.ox.ac.uk>
2016-05-28 13:31:23 +0100

    Specify unwinding depth for each loop in CBMC run script

--- cbmc.sh
+++ cbmc.sh
@@ -1,5 +1,5 @@
 # need cbmc v5.0
-~/cbmc-releases/5.4/cbmc --unwind 3 --no-unwinding-assertions -I . -DCBMC -DPER_CPU_DATA_ARRAY -DFORCE_FAILURE_2 main.c
+~/cbmc-releases/5.4/cbmc --unwind 1 --unwindset rcu_process_callbacks.0:2,rcu_spawn_gp_kthread.0:2,rcu_prepare_cpu.0:2,rcu_init_levelspread.1:2,rcu_init_one.0:2,rcu_init_one.3:2,rcu_init_one.4:2,rcu_init_one.6:3,rcu_init.3:3 -I . -DCBMC -DPER_CPU_DATA_ARRAY -DFORCE_FAILURE_2 main.c
 # Can add "--mm pso" or "--arch powerpc" for some variety.
 # -DFORCE_FAILURE to force failure (bogus dynticks failure)
 # -DFORCE_FAILURE_2 to force failure (drop synchronize_rcu())
